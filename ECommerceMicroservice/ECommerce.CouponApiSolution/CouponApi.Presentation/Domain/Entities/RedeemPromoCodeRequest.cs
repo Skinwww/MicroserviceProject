d7e737bc31046cddf15eeeef27cfdc43cca10cc9
Skinwww <147753979+Skinwww@users.noreply.github.com>
2024-12-21 16:55:02 +0300

    Delete ECommerceMicroservice directory

--- ECommerceMicroservice/ECommerce.CouponApiSolution/CouponApi.Presentation/Domain/Entities/RedeemPromoCodeRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CouponApi.Presentation.Domain.Entities
-{
-    public class RedeemPromoCodeRequest
-    {
-        public int PromoCodeId { get; set; }
-        public int BonusAmount { get; set; }
-        public int UserId { get; set; }
-    }
-}
--- ECommerceMicroservice/ECommerce.SharedLibrarySolution/ECommercelib.SharedLibrary/Responses/Response.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace ECommercelib.SharedLibrary.Responses
-{
-    public record Response(bool Flag = false, string? Message = null);
-  
-}
